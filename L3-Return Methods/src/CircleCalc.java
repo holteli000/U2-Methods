a2f5fce958dd6502fb0cbd199bf095406d91f01b
holteli000 <holteli000@warren.k12.in.us>
2021-09-16 12:49:46 -0400

    most recent work n stuff

--- L3-Return Methods/src/CircleCalc.java
+++ L3-Return Methods/src/CircleCalc.java
@@ -1,15 +1,55 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
+
+    /*
+    1. get the input - radius
+    2. calculate area
+    3. calculate circumference
+    4. display results - output
+     */
 
 
     public static void main(String[] args) {
 
-        //create and run the program
+       double radius = getInput("what is the radius");
+
+       double area = area(radius);
+
+       double circumference = circumference(radius);
+
+       JOptionPane.showMessageDialog(null, displayResult(area, circumference));
+
+
+    }
+     public static double getInput(String prompt){
+        return Double.parseDouble(JOptionPane.showInputDialog(prompt));
+     }
+
+
+
+    //re-write the method to calculate a circle's area using parameters and return statement
+    public static double area(double radius){
+
+        return Math.pow(radius, 2) * Math.PI;
 
     }
 
-    //re-write the method to calculate a circle's area using parameters and return statement
-
-
     //re-write the method to calculate a circle's perimeter using parameters and return statement
+    public static double circumference(double radius){
 
+        return 2 * Math.PI * radius;
+
+    }
+
+    public static String displayResult(double area, double circumference){
+
+        DecimalFormat round = new DecimalFormat("#.00");
+
+        String message = "the area is: " + round.format(area);
+        message += "\nthe circumference is: " + round.format(circumference);
+
+        return message;
+    }
 }
--- L3-Return Methods/src/ReturnMethod.java
+++ L3-Return Methods/src/ReturnMethod.java
@@ -1,4 +1,28 @@
+import javax.swing.*;
+
 public class ReturnMethod {
+
+
+    public static void main(String[] args) {
+
+        double length = Double.parseDouble(JOptionPane.showInputDialog("What is the length"));
+        double width = Double.parseDouble(JOptionPane.showInputDialog("what is the width?"));
+
+        double area = area(length, width);
+
+
+        JOptionPane.showMessageDialog(null, "the area is: " + area + "\nThe perimeter is: " + perimeter(length, width));
+    }
+
+    public static double area(double length, double width) {
+        return length * width;
+
+    }
+   public static double perimeter(double length, double width){
+
+        return 2 * (length + width);
+
+    }
 
 
 }
