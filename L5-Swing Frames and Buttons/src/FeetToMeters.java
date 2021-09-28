ba76ba6a3fb3bc1f0017bf7fb0c387fd6e83a401
holteli000 <holteli000@warren.k12.in.us>
2021-09-28 13:17:46 -0400

    2.5 practice

--- L5-Swing Frames and Buttons/src/FeetToMeters.java
+++ L5-Swing Frames and Buttons/src/FeetToMeters.java
@@ -1,6 +1,59 @@
-public class FeetToMeters {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    /* Create a program with two buttons. Clicking on the first button will prompt the user to enter a distance in feet and
+    public class FeetToMeters {
+
+        public static void main(String[] args) {
+
+            JFrame window = new JFrame("distance conversion");
+            JPanel panel = new JPanel();
+            JButton feetToMeterBut = new JButton("feet to meters");
+            JButton metersToFeetBut = new JButton("meters to feet");
+
+
+            window.setSize(350, 100);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setLocationRelativeTo(null);
+
+
+            feetToMeterBut.addActionListener(new FeetToMeter());
+            metersToFeetBut.addActionListener(new MeterToFeet());
+
+            panel.add(feetToMeterBut);
+            panel.add(metersToFeetBut);
+            window.add(panel);
+
+            window.setVisible(true);
+
+        }
+
+        private static class FeetToMeter implements ActionListener {
+
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                double feet = Double.parseDouble(JOptionPane.showInputDialog("enter the number of feet: "));
+                double meters = feet / 3.28;
+                JOptionPane.showMessageDialog(null, meters + "meters.");
+
+
+            }
+        }
+
+
+        private static class MeterToFeet implements ActionListener {
+
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                double meters = Double.parseDouble(JOptionPane.showInputDialog("enter the number of meters: "));
+                double feet = meters * 3.28;
+                JOptionPane.showMessageDialog(null, feet + "feet.");
+
+
+            }
+
+        }
+    }
+ /* Create a program with two buttons. Clicking on the first button will prompt the user to enter a distance in feet and
     convert it to meters. Clicking on the second button will prompt the user to enter a distance in meters and convert to feet.
      */
-}
