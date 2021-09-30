22aedae8bdf3d9e0afe8076379da35185e3e8d86
holteli000 <holteli000@warren.k12.in.us>
2021-09-30 13:11:15 -0400

    2.6 practice

--- L6-Swing Text/src/BasicCalculator.java
+++ L6-Swing Text/src/BasicCalculator.java
@@ -1,6 +1,75 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class BasicCalculator {
 
-    /* Create a calculator program with two text fields
+    static JTextField inputField1, inputField2;
+    static JLabel resultLabel;
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame("simple calculator");
+        JPanel panel = new JPanel();
+        JLabel inputLabel1 = new JLabel("enter a number: ");
+        JLabel inputLabel2 = new JLabel("enter a number: ");
+        inputField1 = new JTextField(10);
+        inputField2 = new JTextField(10);
+
+        JButton subBut = new JButton("subtract");
+        JButton addBut = new JButton("add");
+        JButton multBut = new JButton("multiply");
+        JButton divBut = new JButton("divide");
+
+        resultLabel = new JLabel ("0.00");
+
+        window.setSize(350,200);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+
+        addBut.addActionListener(new addListener());
+
+
+        panel.add(inputLabel1);
+        panel.add(inputField1);
+        panel.add(inputLabel2);
+        panel.add(inputField2);
+
+        panel.add(resultLabel);
+
+        panel.add(subBut);
+        panel.add(multBut);
+        panel.add(divBut);
+        panel.add(addBut);
+
+        window.add(panel);
+        window.setVisible(true);
+
+    }
+
+    private static class addListener implements ActionListener{
+
+        public void actionPerformed(ActionEvent actionEvent){
+            double number1 = Double.parseDouble(inputField1.getText());
+            double number2 = Double.parseDouble(inputField2.getText());
+
+            resultLabel.setText(String.valueOf(number1 + number2));
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
+ /* Create a calculator program with two text fields
     and 4 buttons, one for each of the arithmetic operations.
 
     The user will enter numbers, could be decimals, then click
@@ -8,4 +77,3 @@ public class BasicCalculator {
 
     NO POP-UPS
      */
-}
